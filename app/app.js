cee536b9d3ddba5ba7583b10d50de1227cf7154b
Amar Vadhia <amar.vadhia@gmail.com>
2019-05-16 11:39:50 -0700

    add code getOptions for modal

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
+// jshint esversion: 6
+
 /*
 
- ### Basic Reqs
+ ### Basic Reqssd
 - [ ] Where to store data? (localstorage)
 - [ ] How to caputure data? (web form)
 - [ ] How to modify data? (update action, delete action)
@@ -13,59 +15,90 @@
 //get item
 var getItem = function(key) {
   return window.localStorage.getItem(key);
-}
+};
 
 //create
 var createItem = function(key, value) {
   return window.localStorage.setItem(key, value);
-}
+};
 
 //update
 var updateItem = function(key, value) {
   return window.localStorage.setItem(key, value);
-}
+};
 
 //delete
 var deleteItem = function(key) {
   return window.localStorage.removeItem(key);
-}
+};
 
 //clear everything
 var clearEverything = function() {
   return window.localStorage.clear();
-}
+};
 
 var keyExists = function(key) {
   var currentValue = getItem(key);
   return currentValue !== null;
-}
+};
+
+var createShowObj = function(watched, total){
+  var showObj = new Object();
+  showObj.watched = watched;
+  showObj.total = total;
+  return JSON.stringify(showObj);
+};
 
 
-///////////////////////////////////////////
+////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
   //preventdefault on button clicks
 $(document).ready(function() {
+  function getOptions(num){
+    for(var i = 1; i <= num; i++){
+      return $('#episodesWatched').append(`<option>${getOptions(i)}</option>`);
+    }
+  }
+  $('.add-shows-button').click(function(){
+    getOptions(10);
+  });
+
   $('#createButton').click(function(event) {
     event.preventDefault();
 
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
+    var showName = $("#showName").val();
+    var episodesWatched = $("#episodesWatched").val();
+    var totalEpisodes = $('#totalEpisodes').val();
+    var showEpisodes = createShowObj(episodesWatched, totalEpisodes);
+    if (keyExists(showName)) {
       //current key exists, do something error-handle-y
     } else {
-      createItem(currentKey, currentValue);
+      createItem(showName, showEpisodes);
+      var parsedEpisodes = JSON.parse(showEpisodes);
+      $('.show-list').prepend(`<div class="inner-content"><li class="${showName}">${showName}: Watched: ${parsedEpisodes.watched} Total: ${parsedEpisodes.total}</li></div>`);
     }
   });
+
 
   $('#updateButton').click(function(event) {
     event.preventDefault();
 
-    var currentKey = $("#keyInput").val();
-    var currentValue = $("#valueInput").val();
-    if (keyExists(currentKey)) {
-      updateItem(currentKey, currentValue);
+    var showName = $("#showName").val();
+    var episodesWatched = $("#episodesWatched").val();
+    var totalEpisodes = $('#totalEpisodes').val();
+    var showEpisodes = createShowObj(episodesWatched, totalEpisodes);
+    if (keyExists(showName)) {
+      updateItem(showName, showEpisodes);
     } else {
       //current key doesnt exist, do stuff
     }
+    var parsedEpisodes = JSON.parse(showEpisodes);
+    $(`.${showName}`).text(`${showName}: Watched: ${parsedEpisodes.watched} Total: ${parsedEpisodes.total}`);
+  });
+
+  $('#clearButton').click(function(event){
+    event.preventDefault();
+
+    clearEverything();
   });
 });
